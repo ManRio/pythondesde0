0ab6e47925b111b7255c8cb8f80f30232bba64e3
ManRio <manureina87@gmail.com>
2025-10-15 12:53:39 +0200

    Bases de datos 2º parte

--- 22-django/AprendiendoDjango/AprendiendoDjango/urls.py
+++ 22-django/AprendiendoDjango/AprendiendoDjango/urls.py
@@ -28,4 +28,10 @@ urlpatterns = [
     path('contacto/', views.contacto, name="contacto"),
     path('contacto/<str:nombre>/', views.contacto, name="contacto"),
     path('contacto/<str:nombre>/<str:apellidos>', views.contacto, name="contacto"),
+    path('crear-articulo/<str:title>/<str:content>/<str:public>', views.crear_articulo, name="crear_articulo"),
+    path('articulo/', views.articulo, name="articulo"),
+    path('articulo/<int:id>/', views.articulo, name="articulo"),
+    path('editar-articulo/<int:id>/', views.editar_articulo, name="editar_articulo"),
+    path('articulos/', views.articulos, name="articulos"),
+    path('borrar-articulo/<int:id>/', views.borrar_articulo, name="borrar_articulo"),
 ]
--- 22-django/AprendiendoDjango/miapp/views.py
+++ 22-django/AprendiendoDjango/miapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
+from miapp.models import Article
+from django.db.models import Q
 
 # Create your views here.
 
@@ -69,3 +71,51 @@ def contacto(request, nombre="", apellidos=""):
         html = "El nombre completo es: "
         html += f"<h3>Contacto de {nombre} {apellidos}</h3>"
     return HttpResponse(layout + f"<h2>Contacto</h2>" + html)
+
+def crear_articulo(request, title, content, public):
+    articulo = Article(
+        title= title,
+        content= content,
+        public= public
+    )
+    articulo.save()
+
+    return HttpResponse(f"Articulo creado: <strong>{articulo.title}</strong> - {articulo.content}")
+
+def articulo(request):
+    try:
+        articulo = Article.objects.get(title="Superman", public=True)
+        response = (f"Artículo: <br/> {articulo.title} - {articulo.content}")
+    except Article.DoesNotExist:
+        response = "<h1>El artículo no existe</h1>"
+    return HttpResponse(response)
+
+def editar_articulo(request, id):
+    articulo = Article.objects.get(pk=id)
+    articulo.title = "Batman"
+    articulo.content = "Pelicula de Batman"
+    articulo.public = True
+    articulo.save()
+    return HttpResponse(f"Artículo editado: <br/> {articulo.title} - {articulo.content}")
+
+def articulos(request):
+    articulos = Article.objects.all().order_by('id')
+
+    articulos = Article.objects.filter(public=True) # ver tambien lookups
+
+    articulos = Article.objects.all().exclude(public=True) # El excluede elimina de la lista los que cumplen la condicion
+
+    articulos = Article.objects.raw('SELECT * FROM miapp_article WHERE public=True ORDER BY id DESC')
+
+    articulos = Article.objects.filter(
+        Q(content__contains="pelicula") | Q(content__contains="serie")
+    )
+
+    return render(request, "articulos.html", {
+        'articulos': articulos
+    })
+
+def borrar_articulo(request, id):
+    articulo = Article.objects.get(pk=id)
+    articulo.delete()
+    return redirect('articulos')
